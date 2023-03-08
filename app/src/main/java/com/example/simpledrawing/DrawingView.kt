337d487894eb04825230e79fa9db0eff552161b2
stasvitalich <stasvitalich1994@gmail.com>
2023-03-08 17:05:20 +0600

    Binding was added to the project. Drawing view was created.

---
+++ app/src/main/java/com/example/simpledrawing/DrawingView.kt
@@ -0,0 +1,41 @@
+package com.example.simpledrawing
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.hardware.camera2.params.BlackLevelPattern
+import android.util.AttributeSet
+import android.view.View
+
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThikcness: Float): Path() {
+
+    }
+}
