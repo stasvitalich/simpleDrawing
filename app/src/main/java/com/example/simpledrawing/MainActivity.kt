41c950b5b1f446fdbc9f7e2e9c8d35df1ea0c114
stasvitalich <stasvitalich1994@gmail.com>
2023-03-13 15:18:09 +0600

    Save button was added.

--- app/src/main/java/com/example/simpledrawing/MainActivity.kt
+++ app/src/main/java/com/example/simpledrawing/MainActivity.kt
@@ -5,6 +5,9 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.app.Instrumentation.ActivityResult
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
@@ -161,5 +164,18 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+    private fun getBitmapFromView(view: View) : Bitmap{
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null){
+            bgDrawable.draw(canvas)
+        } else{
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnedBitmap
     }
 }
